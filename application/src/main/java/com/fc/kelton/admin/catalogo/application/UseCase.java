6831919318e05fbc8e75ede861ef02654f08e2e4
ksf <ksf@ksf.Dlink>
2023-11-12 10:01:31 -0300

    starting project

---
+++ application/src/main/java/com/fc/kelton/admin/catalogo/application/UseCase.java
@@ -0,0 +1,9 @@
+package com.fc.kelton.admin.catalogo.application;
+
+import com.fc.kelton.admin.catalogo.domain.Category;
+
+public class UseCase {
+    public Category execute() {
+        return new Category();
+    }
+}
---
+++ domain/src/main/java/com/fc/kelton/admin/catalogo/domain/Category.java
@@ -0,0 +1,6 @@
+package com.fc.kelton.admin.catalogo.domain;
+
+public class Category {
+
+    public String id;
+}
