cde7dd3c98ec5f98e68f68513530aca86bd98829
ksf <ksf@ksf.Dlink>
2023-11-14 11:21:13 -0300

    more test coverage for create category use case

--- application/src/test/java/com/fc/kelton/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/fc/kelton/admin/catalogo/application/category/create/CreateCategoryUseCaseTest.java
@@ -1,10 +1,13 @@
 package com.fc.kelton.admin.catalogo.application.category.create;
 
 import com.fc.kelton.admin.catalogo.domain.category.CategoryGateway;
+import com.fc.kelton.admin.catalogo.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -12,6 +15,12 @@ import java.util.Objects;
 
 @ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
 
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
@@ -21,12 +30,9 @@ public class CreateCategoryUseCaseTest {
 
         final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
 
-        final CategoryGateway categoryGateway = Mockito.mock(CategoryGateway.class);
 
         Mockito.when(categoryGateway.create(Mockito.any()))
                 .thenAnswer(AdditionalAnswers.returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
 
         final var actualOutput = useCase.execute(aCommand);
 
@@ -45,4 +51,80 @@ public class CreateCategoryUseCaseTest {
                 ));
     }
 
+    @Test
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var aCommand =
+                CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                Assertions.assertThrows(DomainException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(categoryGateway, Mockito.times(0)).create(Mockito.any());
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveCategory_whenCallsCreateCategory_shouldReturnInactiveCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = false;
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+
+        Mockito.when(categoryGateway.create(Mockito.any()))
+                .thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        final var actualOutput = useCase.execute(aCommand);
+
+        Assertions.assertNotNull(actualOutput);
+        Assertions.assertNotNull(actualOutput.id());
+
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(Mockito.argThat(aCategory ->
+                        Objects.equals(expectedName, aCategory.getName())
+                                && Objects.equals(expectedDescription, aCategory.getDescription())
+                                && Objects.equals(expectedIsActive, aCategory.isActive())
+                                && Objects.nonNull(aCategory.getId())
+                                && Objects.nonNull(aCategory.getCreatedAt())
+                                && Objects.nonNull(aCategory.getUpdatedAt())
+                                && Objects.nonNull(aCategory.getDeletedAt())
+                ));
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsRandomException_shouldReturnCategoryId() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "Gateway error";
+
+        final var aCommand = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(categoryGateway.create(Mockito.any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException =
+                Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(aCommand));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getMessage());
+
+        Mockito.verify(categoryGateway, Mockito.times(1))
+                .create(Mockito.argThat(aCategory ->
+                        Objects.equals(expectedName, aCategory.getName())
+                                && Objects.equals(expectedDescription, aCategory.getDescription())
+                                && Objects.equals(expectedIsActive, aCategory.isActive())
+                                && Objects.nonNull(aCategory.getId())
+                                && Objects.nonNull(aCategory.getCreatedAt())
+                                && Objects.nonNull(aCategory.getUpdatedAt())
+                                && Objects.isNull(aCategory.getDeletedAt())
+                ));
+    }
 }
--- domain/src/main/java/com/fc/kelton/admin/catalogo/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/fc/kelton/admin/catalogo/domain/exceptions/DomainException.java
@@ -8,17 +8,17 @@ public class DomainException extends NoStackTraceException {
 
     private final List<Error> errors;
 
-    private DomainException(final List<Error> anErrors) {
-        super("");
+    private DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
     public static DomainException with(final Error anErrors) {
-        return new DomainException(List.of(anErrors));
+        return new DomainException(anErrors.message(), List.of(anErrors));
     }
 
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException("", anErrors);
     }
 
     public List<Error> getErrors() {
