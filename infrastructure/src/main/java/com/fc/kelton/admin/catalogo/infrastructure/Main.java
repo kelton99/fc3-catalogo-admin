a343031c9e3a92503c379725b23893c2250152ce
ksf <ksf@localhost.localdomain>
2023-11-18 11:17:37 -0300

    starting spring boot application

--- infrastructure/src/main/java/com/fc/kelton/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fc/kelton/admin/catalogo/infrastructure/Main.java
@@ -1,5 +1,12 @@
 package com.fc.kelton.admin.catalogo.infrastructure;
 
+import com.fc.kelton.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
-    public static void main(String[] args) { }
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerConfig.class, args);
+    }
 }
---
+++ infrastructure/src/main/java/com/fc/kelton/admin/catalogo/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.fc.kelton.admin.catalogo.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fc.kelton.admin.catalogo")
+public class WebServerConfig {
+}
