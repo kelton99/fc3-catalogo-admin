54c6a32408a0771e537d903193cbee545f0435d5
ksf <ksf@localhost.localdomain>
2023-11-19 12:30:35 -0300

    setting up flyway, creating category jpa entity and first migration

--- domain/src/main/java/com/fc/kelton/admin/catalogo/domain/category/Category.java
+++ domain/src/main/java/com/fc/kelton/admin/catalogo/domain/category/Category.java
@@ -10,7 +10,7 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
     private String name;
     private String description;
     private boolean active;
-    private Instant createdAt;
+    private final Instant createdAt;
     private Instant updatedAt;
     private Instant deletedAt;
 
@@ -37,6 +37,26 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         final var now = Instant.now();
         final var deletedAt = isActive ? null : now;
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
+    }
+
+    public static Category with(
+            final CategoryID anId,
+            final String name,
+            final String description,
+            final boolean active,
+            final Instant createdAt,
+            final Instant updatedAt,
+            final Instant deletedAt
+    ) {
+        return new Category(
+                anId,
+                name,
+                description,
+                active,
+                createdAt,
+                updatedAt,
+                deletedAt
+        );
     }
 
     @Override
