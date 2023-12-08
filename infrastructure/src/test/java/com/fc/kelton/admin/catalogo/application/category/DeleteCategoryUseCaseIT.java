c6440539c45fd0dca87e57d6c3b6c16452e3252d
kelton99 <keltonfreitas53@gmail.com>
2023-12-08 11:09:55 -0300

    delete category use case integrated test

--- infrastructure/src/test/java/com/fc/kelton/admin/catalogo/application/category/DeleteCategoryUseCaseIT.java
+++ infrastructure/src/test/java/com/fc/kelton/admin/catalogo/application/category/DeleteCategoryUseCaseIT.java
@@ -1,54 +1,54 @@
-package com.fc.kelton.admin.catalogo.application.category.delete;
+package com.fc.kelton.admin.catalogo.application.category;
 
+import com.fc.kelton.admin.catalogo.IntegrationTest;
+import com.fc.kelton.admin.catalogo.application.category.delete.DeleteCategoryUseCase;
 import com.fc.kelton.admin.catalogo.domain.category.Category;
 import com.fc.kelton.admin.catalogo.domain.category.CategoryGateway;
 import com.fc.kelton.admin.catalogo.domain.category.CategoryID;
+import com.fc.kelton.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fc.kelton.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
-@ExtendWith(MockitoExtension.class)
-public class DeleteCategoryUseCaseTest {
+import java.util.Arrays;
 
-    @InjectMocks
-    private DefaultDeleteCategoryUseCase useCase;
+@IntegrationTest
+public class DeleteCategoryUseCaseIT {
 
-    @Mock
+    @Autowired
+    private DeleteCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
     private CategoryGateway categoryGateway;
-
-    @BeforeEach
-    public void cleanUp() {
-        Mockito.reset(categoryGateway);
-    }
 
     @Test
     public void givenAValidId_whenCallsDeleteCategory_shouldBeOk() {
         final var aCategory = Category.newCategory("Filmes", "A categoria mais assistida", true);
 
         final var expectedId = aCategory.getId();
+        save(aCategory);
 
-        Mockito.doNothing().when(categoryGateway).deleteById(Mockito.eq(expectedId));
-
+        Assertions.assertEquals(1, categoryRepository.count());
         Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
 
-        Mockito.verify(categoryGateway, Mockito.times(1)).deleteById(expectedId);
+        Assertions.assertEquals(0, categoryRepository.count());
     }
 
     @Test
     public void givenAnInvalidId_whenCallsDeleteCategory_shouldBeOk() {
-       final var expectedId = CategoryID.from("123");
+        final var expectedId = CategoryID.from("123");
 
-        Mockito.doNothing().when(categoryGateway).deleteById(Mockito.eq(expectedId));
+        Assertions.assertEquals(0, categoryRepository.count());
 
         Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
 
-        Mockito.verify(categoryGateway, Mockito.times(1)).deleteById(expectedId);
-
+        Assertions.assertEquals(0, categoryRepository.count());
     }
 
     @Test
@@ -65,5 +65,12 @@ public class DeleteCategoryUseCaseTest {
 
         Mockito.verify(categoryGateway, Mockito.times(1)).deleteById(expectedId);
 
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList());
     }
 }
