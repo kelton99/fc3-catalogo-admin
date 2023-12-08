5fde3d9a92850917971db6eea129a75ce2049446
kelton99 <keltonfreitas53@gmail.com>
2023-12-08 17:23:38 -0300

    document the api

--- infrastructure/src/main/java/com/fc/kelton/admin/catalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/fc/kelton/admin/catalogo/infrastructure/api/controllers/CategoryController.java
@@ -8,9 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CategoryController implements CategoryApi {
 
-
     @Override
-    public ResponseEntity<?> createcategory() {
+    public ResponseEntity<?> createCategory() {
         return null;
     }
 
